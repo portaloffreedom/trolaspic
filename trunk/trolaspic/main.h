e18c3d83abd0708b38b68498ce8a0c5e4a456244
Robenfi <Robenfi@fdb5d8e9-0393-f9f7-d7a8-8f3711ef5682>
2010-07-21 00:41:44 +0000

    Avanzamento di versione: 0.2.1a
    Aggiornato Dijkstra
    	* adesso trova la strada più corta!!!(stavolta davvero)
    	* ho usato la coda con priorita' precostruita
    
    FIXED: L'algoritmo decide a caso le strade fottendosene di te (roberto)

--- trunk/trolaspic/main.h
+++ trunk/trolaspic/main.h
@@ -23,7 +23,7 @@ using namespace std;
     #define PERCORSO "./NetBeansProjects/trolaspic_code/trunk/trolaspic"
 #endif
 
-#define VERSION "0.2.0a - Fillix"
+#define VERSION "0.2.1a - Fillix"
 
 
 struct passaggio_t {
