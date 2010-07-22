fa319f68c1082af46ea629b93f3d94eb0e205c65
matteo.dek <matteo.dek@fdb5d8e9-0393-f9f7-d7a8-8f3711ef5682>
2010-07-22 17:24:14 +0000

    0.9.0a
    - Aggiunta documentazione mancante

--- trolaspic/main.h
+++ trolaspic/main.h
@@ -30,27 +30,51 @@ enum tipo_calcolo {
     per_distanza
 };
 
+/** Struct per contenere tutti i puntatori dei widget e degli oggetti creati
+ * nella finestra principale e necessari alle altre funzioni */
 struct passaggio_t {
+    /** Puntatore alla finestra principale */
     GtkWidget* finestra;
+    /** Puntatore al menu item info nel menu programma */
     GtkWidget* info_i;
+    /** Puntatore al menu item esci nel menu programma */
     GtkWidget* esci_i;
+    /** Puntatore al menu item carica nel menu mappa */
     GtkWidget* carica_i;
+    /** Puntatore al menu item calcola nel menu percoso */
     GtkWidget* calcola_i;
+    /** Puntatore al widget che contiene l'immagine */
     GtkWidget* image;
-    GtkWidget* radio_distanza;
-    GtkWidget* radio_tempo;
+    /** Puntatore al PixBuffer che deve essere caricato dentro il Widget Immagine */
     GdkPixbuf* sfondo;
+    /** Intero che memorizza quanti nodi ci sono caricati nel grafo. Serve alla
+     * finestra di dialogo delle richiesta di calcolo del percorso per limitare
+     * la scelta dei nodi */
     int massimo_numero_nodi;
+    /** Handler_id per il signal connect della funzione ::response_calcola che
+     * serve per potere attivare e disattivera l'handling */
     gulong calcola_id;
+    /** Handler_id per il signal connect della funzione ::response_non_calcola che
+     * serve per potere attivare e disattivera l'handling */
     gulong errore_id;
 };
 
+/** Struct per contenere tutti i puntatori dei widget e delle informazioni create
+ * dalla finestra di calcolo percorso e necessarie per avviare il calcolo del
+ * percorso */
 struct passaggio_t2 {
+    /** Puntatore alla finestra di dialogo della scelta del percorso */
     GtkWidget* finestra;
+    /** Puntatore al radio-pulsante che sceglie di calcolare il percorso per distanza */
     GtkWidget* radio_distanza;
+    /** Puntatore al radio-pulsante che sceglie di calcolare il percorso per tempo */
     GtkWidget* radio_tempo;
+    /** Puntatore al widget di scelta dell'indice del nodo di partenza */
     GtkWidget* partenza_t;
+    /** Puntatore al widget di scelta dell'indice del nodo di arrivo */
     GtkWidget* arrivo_t;
+    /** Variabile utilizzata per memorizzarci quale percorso verrà utilizzato
+     * nel calcolare la strada più breve */
     tipo_calcolo t_calcolo;
 };
 
